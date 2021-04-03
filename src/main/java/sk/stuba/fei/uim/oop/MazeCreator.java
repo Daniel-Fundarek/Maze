f786a4649f903a2e7d31ab521e0099961c7fc568
Daniel-Fundarek <79155394+Daniel-Fundarek@users.noreply.github.com>
2021-04-03 15:53:55 +0200

    junky maze

--- src/main/java/sk/stuba/fei/uim/oop/MazeCreator.java
+++ src/main/java/sk/stuba/fei/uim/oop/MazeCreator.java
@@ -10,7 +10,7 @@ public class MazeCreator {
         createBorder();
 
         createMaze(1,1);
-        printMaze();
+
     }
 
     private void createBorder(){
@@ -38,10 +38,9 @@ public class MazeCreator {
 
     }
     private void createMaze(int row, int column){
-        if (maze[row][column] == 0){
-            maze[row][column]= 9;
-            clearWall(row,column);
-        }
+
+        clearWall(row,column);
+
     }
     private boolean checkWall(int row, int column){
         boolean wall = false;
@@ -52,29 +51,47 @@ public class MazeCreator {
 
     }
     private void clearWall(int row, int column){
-        ArrayList<ArrayList<Integer>> positionOfWall = new ArrayList<ArrayList<Integer>>();
-        for(int i =0; i<4;i++) {
-            positionOfWall.add(new ArrayList<Integer>());
-        }
-        positionOfWall.get(0).add(0,-1);
-        positionOfWall.get(0).add(1, 0);
+        System.out.println();
+        if (row<= maze.length && column <= maze[0].length && maze[row][column] == 0) {
+            maze[row][column] = 9;
+            printMaze();
 
-        positionOfWall.get(1).add(0,1);
-        positionOfWall.get(1).add(1, 0);
+            ArrayList<ArrayList<Integer>> positionOfWall = new ArrayList<ArrayList<Integer>>();
+            for (int i = 0; i < 4; i++) {
+                positionOfWall.add(new ArrayList<Integer>());
+            }
+            positionOfWall.get(0).add(0, -1);
+            positionOfWall.get(0).add(1, 0);
 
-        positionOfWall.get(2).add(0,0);
-        positionOfWall.get(2).add(1, -1);
+            positionOfWall.get(1).add(0, 1);
+            positionOfWall.get(1).add(1, 0);
 
-        positionOfWall.get(3).add(0,0);
-        positionOfWall.get(3).add(1, 1);
+            positionOfWall.get(2).add(0, 0);
+            positionOfWall.get(2).add(1, -1);
 
-        for (int rowOfArray = 0; rowOfArray < positionOfWall.size(); rowOfArray++){
-                if(!checkWall(row + positionOfWall.get(rowOfArray).get(0), column + positionOfWall.get(rowOfArray).get(1))){
+            positionOfWall.get(3).add(0, 0);
+            positionOfWall.get(3).add(1, 1);
+
+            for (int rowOfArray = 0; rowOfArray < positionOfWall.size(); rowOfArray++) {
+                if (!checkWall(row + positionOfWall.get(rowOfArray).get(0), column + positionOfWall.get(rowOfArray).get(1))) {
                     positionOfWall.remove(rowOfArray);
-
+                    rowOfArray--;
                 }
+            }
+            if (positionOfWall.size()>0) {
+                int randomWall = random.nextInt(positionOfWall.size());
+                for (int i = 0; i < positionOfWall.size(); i++) {
+                    if (randomWall == i) {
+                        maze[row + positionOfWall.get(i).get(0)][column + positionOfWall.get(i).get(1)] = 5;
+                    } else {
+                        maze[row + positionOfWall.get(i).get(0)][column + positionOfWall.get(i).get(1)] = 1;
+                    }
+                }
+                System.out.println(positionOfWall.get(randomWall).get(0)+ "  " + positionOfWall.get(randomWall).get(1));
+                System.out.println(positionOfWall);
+                clearWall(row + 2*positionOfWall.get(randomWall).get(0),column + 2*positionOfWall.get(randomWall).get(1));
+            }
         }
-        int randomWall = random.nextInt(positionOfWall.size());
-        maze[row+positionOfWall.get(randomWall).get(0)][column+positionOfWall.get(randomWall).get(1)] = 1;
+
     }
 }
